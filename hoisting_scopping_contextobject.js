54cfd2d7aae517e72936c4e615ab9dd1f4a84b41
Everton Sávio S. Lucas <Havyx@users.noreply.github.com>
2020-01-25 12:46:11 -0300

    ups

---
+++ hoisting_scopping_contextobject.js
@@ -0,0 +1,77 @@
+///////////////////////////////////////
+// Hoisting
+
+//function declaration
+calcular(1965);
+
+function calcular(year) {
+  console.log(2019 - year);
+}
+
+/* retirement(1990); */
+//funcion expression
+var retirement = function(year) {
+  console.log(65 - (2020 - year));
+};
+
+console.log(age);
+var age = 23;
+console.log(age);
+
+function foo() {
+  var age = 65;
+  console.log(age);
+}
+
+foo();
+console.log(age);
+
+var a = "Var a";
+//scopping
+function escop_chain() {
+  console.log(a);
+  var b = "Var b";
+  escop();
+  function escop() {
+    console.log(a + b);
+  }
+}
+
+escop_chain();
+
+// this window object
+console.log(this);
+//Window {parent: Window, opener: null, top: Window, length: 0, frames: Window,
+
+/* calculateAge(1985);
+
+function calculateAge(year) {
+  console.log(2016 - year);
+  console.log(this);
+} */
+//Window {parent: Window, opener: null, top: Window, length: 0, frames: Window,
+
+var issoeumobjeto = {
+  name: "Savio",
+  yearofbirth: 1987,
+  calculateAge: function() {
+    console.log(this);
+    //{name: "savio", yearofbirth: 1987, calculateAge: ƒ}
+    console.log(2020 - this.yearofbirth);
+    function innerFunc() {
+      console.log(this);
+      //Window {parent: Window, opener: null, top: Window, length: 0, frames: Window, …}
+    }
+    innerFunc();
+  }
+};
+issoeumobjeto.calculateAge();
+//{name: "savio", yearofbirth: 1987, calculateAge: ƒ}
+
+var susy = {
+  name: "Suzy",
+  yearofbirth: 1990
+};
+susy.calculateAge = issoeumobjeto.calculateAge;
+
+susy.calculateAge(1900);
