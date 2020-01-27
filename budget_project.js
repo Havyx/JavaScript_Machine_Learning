4c530998cd6a242a31d96eb425d93a0ed8cf0f65
havyx <everluca@hotmail.com>
2020-01-27 15:46:47 -0300

    novos_js

---
+++ budget_project.js
@@ -0,0 +1,112 @@
+//Budget controller
+var budgetController = (function() {
+  //comeca com maisculo a class / construtor
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem;
+      if (data.allItems[type].lenght > 0) {
+        ID = data.allItems[type][data.allItems[type].lenght - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      if (type == 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type == 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      data.allItems[type].push(newItem);
+      return newItem;
+    },
+    //testing: function() {
+      //console.log(data);
+    },
+  };
+})();
+
+//UI
+var UIController = (function() {
+  var DOMstrings = {
+    input_type: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.input_type).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    },
+  };
+})();
+
+//Global APP controle -> diz os outros modulos o que fazer get and set outros controles
+var controle = (function(budgetCtrl, UICtrl) {
+  var seturpEventListeners = function() {
+    //quando voce passa uma funcao como argumeto nao coloca () pois () e para executar(callback function)
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    // Global listenning e -> its event
+
+    document.addEventListener('keypress', function(e) {
+      if (e.keyCode === 13 || event.which == 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+  var DOM = UICtrl.getDOMstrings();
+  //for DRY principles
+  var ctrlAddItem = function() {
+    //1. get filed input data
+    var input = UICtrl.getInput();
+    //2. add item to budget controll
+    var new_item = budgetCtrl.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
+    //3. user interface get new item
+    //4. calculate budget
+    //5. display the budget
+    console.log(input);
+  };
+
+  return {
+    init: function() {
+      console.log('Iniciado...'), seturpEventListeners();
+    },
+  };
+})(budgetController, UIController);
+
+controle.init();
