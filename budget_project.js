0a46c3b6e6e4e8df5c46acc605a3056870526f03
havyx <everluca@hotmail.com>
2020-01-27 16:38:44 -0300

    Ups

--- budget_project.js
+++ budget_project.js
@@ -6,13 +6,11 @@ var budgetController = (function() {
     this.description = description;
     this.value = value;
   };
-
   var Income = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
-
   var data = {
     allItems: {
       exp: [],
@@ -23,7 +21,6 @@ var budgetController = (function() {
       inc: 0,
     },
   };
-
   return {
     addItem: function(type, des, val) {
       var newItem;
@@ -32,18 +29,13 @@ var budgetController = (function() {
       } else {
         ID = 0;
       }
-
       if (type == 'exp') {
         newItem = new Expense(ID, des, val);
       } else if (type == 'inc') {
         newItem = new Income(ID, des, val);
       }
-
       data.allItems[type].push(newItem);
       return newItem;
-    },
-    //testing: function() {
-      //console.log(data);
     },
   };
 })();
@@ -55,6 +47,8 @@ var UIController = (function() {
     inputDescription: '.add__description',
     inputValue: '.add__value',
     inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list',
   };
 
   return {
@@ -64,6 +58,26 @@ var UIController = (function() {
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value,
       };
+    },
+
+    addListItem: function(obj, type) {
+      var html, newHtml, element;
+
+      if (type == 'inc') {
+        element = DOMstrings.incomeContainer;
+        html =
+          '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type == 'exp') {
+        element = DOMstrings.expensesContainer;
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
     getDOMstrings: function() {
